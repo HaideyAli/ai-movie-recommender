4d55a59d5a9a176436a128b7664b166a8f634e96
Haider Ali <haiderali1248@outlook.com>
2026-01-28 23:33:47 -0600

    Removed part of run command in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,8 @@
-import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from recommendations import router as recommendations_router
 
 app = FastAPI()
-
-origins = [
-    "http://localhost:3000",
-    "https://flick-finder-green.vercel.app"
-]
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,13 +13,3 @@ app.add_middleware(
 )
 
 app.include_router(recommendations_router, prefix="/recommendations")
-
-# ---- RUN FASTAPI ----
-if __name__ == "__main__":
-    import uvicorn
-
-    # Railway provides PORT in env variables
-    port = int(os.environ.get("PORT", 8000))
-
-    # host=0.0.0.0 is necessary for cloud deployment
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
